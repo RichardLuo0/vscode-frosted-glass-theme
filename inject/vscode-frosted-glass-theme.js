4c78fd4b2df5daec418d0701396440cf2a87372e
RichardLuo <22096448+RichardLuo0@users.noreply.github.com>
2023-09-03 15:27:27 +0800

    fix: it just works ver 3. searchEditor and minimap still aren't working

--- inject/vscode-frosted-glass-theme.js
+++ inject/vscode-frosted-glass-theme.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 (function () {
   const useThemeColor = true;
   const opacity = 0.4;
@@ -51,6 +52,7 @@
       "--vscode-debugToolBar-background",
       "--vscode-editorHoverWidget-statusBarBackground",
       "--vscode-editorStickyScroll-background",
+      "--vscode-tab-unfocusedInactiveBackground",
       "--vscode-tab-inactiveBackground",
       "--vscode-breadcrumb-background",
     ];
@@ -118,6 +120,7 @@
       // Replace `outline` with `border`
       if (scrollableElement.style.outline.length !== 0) {
         scrollableElement.style.border = scrollableElement.style.outline;
+        scrollableElement.style.margin = "-1px";
         scrollableElement.style.outline = "";
       }
       // Fix sub menu
@@ -192,6 +195,63 @@
     }
   });
 
+  function findPropertyWith(object, condition) {
+    for (const key in object) {
+      const value = object[key];
+      if (condition(value)) {
+        return { key, value };
+      }
+    }
+  }
+
+  function createBackdropCodeEditorWidget(CodeEditorWidget) {
+    return class extends CodeEditorWidget {
+      constructor(domElement) {
+        super(...arguments);
+        domElement.style.position = "absolute";
+        domElement.style.top = "0px";
+        proxy(domElement, "appendChild", (monacoEditor) => {
+          const overlayWidgets =
+            monacoEditor.querySelector("div.overlayWidgets");
+          overlayWidgets.style.position = "relative";
+          overlayWidgets.style.top = "57px";
+        });
+      }
+
+      setModel() {
+        super.setModel(...arguments);
+        this.changeViewZones((accessor) => {
+          const domNode = document.createElement("div");
+          this._viewZoneId = accessor.addZone({
+            heightInPx: 57,
+            afterLineNumber: 0,
+            domNode,
+          });
+          this.setScrollTop(-57);
+        });
+      }
+    };
+  }
+
+  function replaceCodeEditorWidgetInArguments(arguments) {
+    let codeEditorWidgetPair;
+    const codeEditorWidgetIndex = Array.prototype.findLastIndex.call(
+      arguments,
+      (exports) => {
+        codeEditorWidgetPair = findPropertyWith(
+          exports,
+          (c) => c.prototype?.changeViewZones
+        );
+        return codeEditorWidgetPair;
+      }
+    );
+    if (codeEditorWidgetIndex >= 0 && codeEditorWidgetPair !== undefined) {
+      const { key, value } = codeEditorWidgetPair;
+      arguments[codeEditorWidgetIndex][key] =
+        createBackdropCodeEditorWidget(value);
+    }
+  }
+
   Object.defineProperty(globalThis, "define", {
     get() {
       return this._define;
@@ -201,35 +261,20 @@
         apply: function (target, thisArg, argumentsList) {
           const oldExport = argumentsList[2];
           switch (argumentsList[0]) {
-            case "vs/editor/browser/widget/codeEditorWidget":
+            case "vs/workbench/browser/parts/editor/textCodeEditor":
               argumentsList[2] = function (_require, exports) {
+                replaceCodeEditorWidgetInArguments(arguments);
+
                 oldExport.call(this, ...arguments);
-                let theKey, CodeEditorWidget;
-                for (const key in exports) {
-                  theKey = key;
-                  CodeEditorWidget = exports[key];
-                  break;
-                }
-                exports[theKey] = class extends CodeEditorWidget {
-                  constructor(domElement) {
-                    super(...arguments);
-                    domElement.style.position = "absolute";
-                    domElement.style.top = "0px";
-                  }
 
-                  setModel() {
-                    super.setModel(...arguments);
-                    this.changeViewZones((accessor) => {
-                      const domNode = document.createElement("div");
-                      this._viewZoneId = accessor.addZone({
-                        heightInPx: 57,
-                        afterLineNumber: 0,
-                        domNode,
-                      });
-                      this.setScrollTop(-57);
-                    });
-                  }
-
+                const { key, value: AbstractTextCodeEditor } = findPropertyWith(
+                  exports,
+                  (value) =>
+                    typeof value === "function" &&
+                    !Object.getOwnPropertyDescriptor(value, "prototype")
+                      ?.writable
+                );
+                exports[key] = class extends AbstractTextCodeEditor {
                   layout(dimension) {
                     return super.layout(
                       dimension.with
@@ -240,30 +285,30 @@
                 };
               };
               break;
-            case "vs/editor/browser/view":
+            case "vs/editor/browser/widget/diffEditorWidget":
               argumentsList[2] = function (_require, exports) {
                 oldExport.call(this, ...arguments);
-                let theKey, View;
-                for (const key in exports) {
-                  theKey = key;
-                  View = exports[key];
-                  break;
-                }
-                exports[theKey] = class extends View {
-                  constructor() {
+
+                const { key, value: DiffEditorWidget } = findPropertyWith(
+                  exports,
+                  (value) =>
+                    typeof value === "function" &&
+                    !Object.getOwnPropertyDescriptor(value, "prototype")
+                      ?.writable
+                );
+                exports[key] = class extends DiffEditorWidget {
+                  constructor(domElement) {
                     super(...arguments);
-                    for (const key in this) {
-                      if (
-                        this[key] &&
-                        this[key].addWidget &&
-                        this[key].getDomNode
-                      ) {
-                        const _overlayWidgetsDom = this[key].getDomNode();
-                        _overlayWidgetsDom.setPosition("relative");
-                        _overlayWidgetsDom.setTop(57);
-                        break;
-                      }
-                    }
+                    domElement.style.position = "absolute";
+                    domElement.style.top = "0px";
+                  }
+
+                  layout(dimension) {
+                    return super.layout(
+                      dimension.with
+                        ? dimension.with(undefined, window.innerHeight)
+                        : dimension
+                    );
                   }
                 };
               };
