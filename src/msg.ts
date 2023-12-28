07a8b8049c516917fb7ba80f269d6a95f29a6eb7
RichardLuo0 <22096448+RichardLuo0@users.noreply.github.com>
2023-12-28 17:31:38 +0800

    docs: remove "manual restart" part

--- src/msg.ts
+++ src/msg.ts
@@ -1,14 +1,13 @@
 export const msg = {
-  tryAdmin: "Require admin privileges to apply changes. Select `Try as Admin` to try with admin privileges.",
+  tryAdmin:
+    "Require admin privileges to apply changes. Click `Try as Admin` to try with admin privileges.",
   admin: "Run VS Code with admin privileges so the changes can be applied.",
-  enabled:
-    "Frosted glass theme enabled. Restart to take effect. " +
-    "If Code complains about it is corrupted, CLICK DON'T SHOW AGAIN. " +
-    "See README for more detail.",
-  applied:
-    "Changes have been applied. Please do a manual restart. " +
-    '"Developer: Reload Window" sometimes does not work. ' +
-    "See README for more detail.",
+  enabled: `Frosted glass theme enabled. Restart to take effect.
+If Code complains about it is corrupted, CLICK DON'T SHOW AGAIN.
+See README for more detail.`,
+  applied: `Changes have been applied. Restart to take effect.
+\`Developer: Reload Window\` sometimes does not work.
+See README for more detail.`,
   configChanged: "Detected config change. Click the button to apply.",
   disabled:
     "Frosted glass theme disabled and reverted to default. Restart to take effect.",
@@ -16,7 +15,7 @@ export const msg = {
   somethingWrong: "Something went wrong: ",
   restartIde: "Restart Visual Studio Code",
   applyChanges: "Apply Changes",
-  tryAsAdmin:"Try as Admin",
+  tryAsAdmin: "Try as Admin",
   notfound: "Frosted glass theme not found.",
   notConfigured: "Frosted glass theme path not configured. ",
   reloadAfterVersionUpgrade:
