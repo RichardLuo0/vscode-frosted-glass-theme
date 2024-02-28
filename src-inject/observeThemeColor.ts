584d965b391497ea89ec23ce160e42c4c07eee7d
RichardLuo <22096448+RichardLuo0@users.noreply.github.com>
2024-02-28 15:45:44 +0800

    feat: add `--vscode-menu-separatorBackground`

--- src-inject/observeThemeColor.ts
+++ src-inject/observeThemeColor.ts
@@ -12,6 +12,7 @@ const colorVarMap = new Map([
   ["--vscode-editorWidget-border", opacity.border],
   ["--vscode-editorHoverWidget-border", opacity.border],
   ["--vscode-editorSuggestWidget-border", opacity.border],
+  ["--vscode-menu-separatorBackground", opacity.border],
 ]);
 
 const backgroundVarList = [
