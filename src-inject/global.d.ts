d44a5c8b403331e551045573ca17a828f5375d6d
RichardLuo <22096448+RichardLuo0@users.noreply.github.com>
2024-05-01 17:48:06 +0800

    chore: export css as `CSSStyleSheet` and use `resolveJsonModule`

--- src-inject/global.d.ts
+++ src-inject/global.d.ts
@@ -1,31 +1,4 @@
-declare module "*.css";
-declare module "*.json" {
-  const config: {
-    backdropFilter: string;
-    transition: string;
-    opacity: {
-      menu: number;
-      selection: number;
-      border: number;
-      separator: number;
-      minimap: number;
-      panelHeader: number;
-    };
-    animation: {
-      menu: string;
-      dialog: string;
-    };
-    revealEffect: {
-      enabled: boolean;
-      opacity: number;
-      gradientSize: number;
-      clickEffect: boolean;
-    };
-    borderRadius: {
-      menu: string;
-      menuItem: string;
-      suggestWidget: string;
-    };
-  };
-  export default config;
+declare module "*.css" {
+  const styleSheet: CSSStyleSheet;
+  export default styleSheet;
 }
