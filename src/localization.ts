6e4f551959635e7ad0b0953b3acf6d0b098a51e1
RichardLuo <22096448+RichardLuo0@users.noreply.github.com>
2024-09-04 14:32:11 +0800

    perf(i18n): improve logic

--- src/localization.ts
+++ src/localization.ts
@@ -1,21 +1,18 @@
 import { readFileSync } from "fs";
 import path from "path";
 
-let dictionary: { [key: string]: string | undefined };
+const acceptLang = new Set(["en", "zh-cn"]);
 const config = JSON.parse(process.env.VSCODE_NLS_CONFIG as string);
 const i18nPath = path.join(__dirname, "../i18n");
-try {
-  dictionary = JSON.parse(
-    readFileSync(
-      path.join(i18nPath, `bundle.i18n.${config.locale}.json`),
-      "utf-8"
-    )
-  );
-} catch (error) {
-  dictionary = JSON.parse(
-    readFileSync(path.join(i18nPath, `bundle.i18n.en.json`), "utf-8")
-  );
-}
+const dictionary: { [key: string]: string | undefined } = JSON.parse(
+  readFileSync(
+    path.join(
+      i18nPath,
+      `bundle.i18n.${acceptLang.has(config.locale) ? config.locale : "en"}.json`
+    ),
+    "utf-8"
+  )
+);
 
 export function localize(key: string, ...args: string[]) {
   const translated = dictionary[key];
