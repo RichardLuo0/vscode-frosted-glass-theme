1d39167f6fbc013ceac4ec3ede570ed484de082e
RichardLuo <22096448+RichardLuo0@users.noreply.github.com>
2025-05-04 20:50:33 +0800

    perf: improve load svgs

--- src-inject/loadSvg.ts
+++ src-inject/loadSvg.ts
@@ -12,36 +12,33 @@ const ttp = trustedTypes.createPolicy("fgtSvg", {
   },
 });
 
-export function loadSvgs(svgList: string[]) {
-  let fetchList = [];
-  for (const svg of svgList) {
-    fetchList.push(fetch(makeAbsolutePath(svg)));
-  }
+export type MountSvgTo = (
+  element: Node & ParentNode,
+  copy?: boolean
+) => Promise<void>;
+
+export function loadSvgs(svgList: string[]): MountSvgTo {
+  const fetchList = svgList.map(path =>
+    fetch(makeAbsolutePath(path)).then(async res => {
+      const svgStr = await res.text();
+      const svg = new DOMParser()
+        .parseFromString(ttp.createHTML(svgStr), "text/xml")
+        .querySelector<SVGElement>("svg");
+      if (!svg) throw res.url + " does not contain a valid svg!";
+      svg.style.position = "absolute";
+      svg.style.width = "0px";
+      svg.style.height = "0px";
+      svg.style.colorInterpolation = "srgb";
+      svg.style.colorInterpolationFilters = "srgb";
+      return svg;
+    })
+  );
   return async function mountSvgTo(
     element: Node & ParentNode,
     copy: boolean = false
   ) {
-    let _this = mountSvgTo as typeof mountSvgTo & { svgs?: Node[] };
-    let justCreated = false;
-    if (!_this.svgs) {
-      justCreated = true;
-      _this.svgs = [];
-      for (const res of await Promise.all(fetchList)) {
-        const svgStr = await res.text();
-        const svg = new DOMParser()
-          .parseFromString(ttp.createHTML(svgStr), "text/xml")
-          .querySelector<SVGElement>("svg");
-        if (!svg) throw res.url + " does not contain a valid svg!";
-        svg.style.position = "absolute";
-        svg.style.width = "0px";
-        svg.style.height = "0px";
-        svg.style.colorInterpolation = "srgb";
-        svg.style.colorInterpolationFilters = "srgb";
-        _this.svgs.push(svg);
-      }
-    }
-    if (copy || !justCreated)
-      for (const svg of _this.svgs) element.appendChild(svg.cloneNode(true));
-    else element.append(..._this.svgs);
+    const svgList = await Promise.all(fetchList);
+    if (copy) svgList.forEach(svg => element.appendChild(svg.cloneNode(true)));
+    else element.append(...svgList);
   };
 }
