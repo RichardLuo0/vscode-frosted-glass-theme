ac4fe99d8a06f4bb8ede1a37072afcfd21ea7485
RichardLuo <22096448+RichardLuo0@users.noreply.github.com>
2026-08-01 14:50:00 +0800

    fix: vscode will not fail to launch when file doesn't exist

--- src/InjectionAdmin.ts
+++ src/InjectionAdmin.ts
@@ -21,7 +21,7 @@ function expandEnv(env: { [key: string]: string }): string {
 export default class InjectionAdmin implements IInjection {
   constructor(
     private files: string[],
-    private htmlFile: string
+    private baseFile: string
   ) {}
 
   async inject(): Promise<void> {
@@ -51,7 +51,7 @@ export default class InjectionAdmin implements IInjection {
           process.execPath
         }" "${__dirname}/InjectionAdminMain.js" --no-sandbox ${funcName} "${escape(
           JSON.stringify(this.files)
-        )}" "${this.htmlFile}"`,
+        )}" "${this.baseFile}"`,
         { name: "Frosted Glass Theme", env },
         error => {
           if (error) reject(error);
--- src/InjectionNormal.ts
+++ src/InjectionNormal.ts
@@ -27,7 +27,7 @@ class HtmlPatcher implements Patcher {
       .replace(
         /(<\/html>)/,
         `<!-- !! VSCODE-FROSTED-GLASS-THEME-SESSION-ID ${uuid} !! -->\n` +
-          `<!-- !! VSCODE-FROSTED-GLASS-THEME-START !! -->\n${injection}\n<!-- !! VSCODE-FROSTED-GLASS-THEME-END !! -->\n</html>`
+          `<!-- !! VSCODE-FROSTED-GLASS-THEME-START !! -->\n${injection}<!-- !! VSCODE-FROSTED-GLASS-THEME-END !! -->\n</html>`
       );
 
     await fs.promises.writeFile(this.file, content, "utf-8");
@@ -60,7 +60,7 @@ class HtmlPatcher implements Patcher {
     let res = "";
     for (const item of files) {
       const imp = this.computeInjectedHTMLItem(item);
-      if (imp) res += imp;
+      if (imp) res += imp + "\n";
     }
     return res;
   }
@@ -87,13 +87,13 @@ class JsPatcher implements Patcher {
   constructor(private file: string) {}
 
   async patch(uuid: string, files: string[]) {
-    const injection = this.computeInjectedHTML(files);
+    const injection = this.computeInjectedJs(files);
 
     let content = fs.readFileSync(this.file, "utf-8");
     content = this.clearExistingPatches(content);
 
     content =
-      `//VSCODE-FROSTED-GLASS-THEME-SESSION-ID ${uuid}\n//VSCODE-FROSTED-GLASS-THEME-START\n${injection}\n//VSCODE-FROSTED-GLASS-THEME-END\n` +
+      `//VSCODE-FROSTED-GLASS-THEME-SESSION-ID ${uuid}\n//VSCODE-FROSTED-GLASS-THEME-START\n${injection}//VSCODE-FROSTED-GLASS-THEME-END\n` +
       content;
 
     await fs.promises.writeFile(this.file, content, "utf-8");
@@ -120,20 +120,22 @@ class JsPatcher implements Patcher {
       .replace(/\/\/VSCODE-FROSTED-GLASS-THEME-SESSION-ID [\w-]+\n/g, "");
   }
 
-  protected computeInjectedHTML(files: string[]) {
-    let res = "";
+  protected computeInjectedJs(files: string[]) {
+    let res = "try {";
     for (const item of files) {
-      const imp = this.computeInjectedHTMLItem(item);
-      if (imp) res += imp;
+      const imp = this.computeInjectedJsItem(item);
+      if (imp) res += imp + "\n";
     }
-    return res;
+    return res + `} catch(err) {
+  console.error('failed to load frosted glass theme: ', err);
+};`;
   }
 
-  protected computeInjectedHTMLItem(url: string) {
+  protected computeInjectedJsItem(url: string) {
     url =
       "file://" +
       (process.platform === "win32" ? "/" + url : url).replace(/\\/g, "/");
-    return `import "${url}";`;
+    return `await import("${url}");`;
   }
 }
 
